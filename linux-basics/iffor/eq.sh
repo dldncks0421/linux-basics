d224d410b25b1f93842c6388645b9d4f8424d722
dldncks0421 <zxc1097@naver.com>
2024-04-17 12:44:46 +0900

    ㅎ

--- linux-basics/iffor/eq.sh
+++ linux-basics/iffor/eq.sh
@@ -1,7 +1,8 @@
-if [ ! 1 -eq 2 ]
+#!/bin/sh
+if [ 10 -eq 10 ]
 then
-    echo "1과 2는 같지 않습니다."
+    echo "10은 10과 같습니다."
 else
-    echo "1과 2는 같습니다."
+    echo "10은 10과 같지 않습니다."
 fi
 exit 0
---
+++ linux-basics/name.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo '사용자 이름 : ' $USER
+echo 'host name : ' $HOSTNAME
+echo '홈디렉토리 : ' $HOME
+exit 0 
